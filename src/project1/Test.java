4573fb8d9a26fd0a1dcaeb56d31962620cecb789
ar-vivek96 <vivekyadhav96@gmail.com>
2024-10-04 18:34:43 +0530

    more changes

--- src/project1/Test.java
+++ src/project1/Test.java
@@ -7,7 +7,7 @@ public class Test {
 		if(hungry=="yes"|| hungry=="y"||hungry=="Y")
 			System.out.println("Eat something");
 		else
-			System.out.println("Do some Work");
+			System.out.println("Do some Work and when you feel hungry just go and eat");
 			
 			
 
